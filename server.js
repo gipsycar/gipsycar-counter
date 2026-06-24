28e98429eee3b381a5af102eeadb36fb570c6442
József Kopár <pmhazepito@gmail.com>
2026-06-24 11:47:53 +0200

    Serve static files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
-
+app.use(express.static(__dirname));
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
